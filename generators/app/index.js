ca0695600f3d64e49959e62d1eb7c5be7ad159ae
Viktor Zavala <viktor@acklenavenue.com>
2015-11-10 16:58:00 -0600

    working indx.js

--- generators/app/index.js
+++ generators/app/index.js
@@ -1,15 +1,10 @@
 var generators = require('yeoman-generator');
 
 module.exports = generators.Base.extend({
-  // The name `constructor` is important here
-  constructor: function () {
-    generators.Base.apply(this, arguments);
-
     method1: function () {
       console.log('method 1 just ran');
     },
-    method2: function () {
+    method2: function() {
       console.log('method 2 just ran');
     }
-  }
-});
+  });
