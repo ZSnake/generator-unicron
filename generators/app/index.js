a577def94842a07e4141b9c491711823d7ec6945
Viktor Zavala <viktor@acklenavenue.com>
2015-11-10 18:06:33 -0600

    added a few questions that dont do crap yet

--- generators/app/index.js
+++ generators/app/index.js
@@ -1,10 +1,30 @@
 var generators = require('yeoman-generator');
+var _ = require('lodash');
 
 module.exports = generators.Base.extend({
-    method1: function () {
-      console.log('method 1 just ran');
-    },
-    method2: function() {
-      console.log('method 2 just ran');
-    }
+      constructor: function () {
+        generators.Base.apply(this, arguments);
+
+        // This makes `appname` a required argument.
+        this.argument('appname', { type: String, required: true });
+        // And you can then access it later on this way; e.g. CamelCased
+        this.appname = _.camelCase(this.appname);
+      },
+      prompting: function () {
+        var done = this.async();
+        this.prompt([{
+          type    : 'input',
+          name    : 'appname',
+          message : 'Your application name: ',
+          default: this.appname
+        },{
+          type    : 'input',
+          name    : 'database',
+          message : 'Your database name: ',
+          default : this.appname + "db"
+        }], function (answers) {
+          this.log(answers.database);
+          done();
+        }.bind(this));
+      }
   });
